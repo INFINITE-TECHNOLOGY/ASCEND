f7fdc32ef9d843d7e31541da25e5f18bebea393f
apryamostanov <anton.pryamostanov@gmail.com>
2020-04-29 02:27:35 +0300

    Progress

---
+++ ascend-sdk/src/main/groovy/io/infinite/ascend/granting/server/authentication/SmsOtpValidator.groovy
@@ -0,0 +1,54 @@
+package io.infinite.ascend.granting.server.authentication
+
+import groovy.util.logging.Slf4j
+import io.infinite.ascend.common.entities.Authentication
+import io.infinite.ascend.common.entities.Authorization
+import io.infinite.ascend.common.exceptions.AscendUnauthorizedException
+import io.infinite.ascend.common.services.JwtService
+import io.infinite.ascend.granting.server.entities.TrustedPublicKey
+import io.infinite.ascend.granting.server.repositories.TrustedPublicKeyRepository
+import io.infinite.blackbox.BlackBox
+import io.infinite.carburetor.CarburetorLevel
+import io.infinite.http.HttpRequest
+import io.infinite.http.SenderDefaultHttps
+import org.apache.commons.lang3.time.FastDateFormat
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+
+@BlackBox(level = CarburetorLevel.METHOD)
+@Slf4j
+@Service
+class SmsOtpValidator implements AuthenticationValidator {
+
+    @Autowired
+    JwtService jwtService
+
+    @Autowired
+    TrustedPublicKeyRepository trustedAppRepository
+
+    SenderDefaultHttps senderDefaultHttps = new SenderDefaultHttps()
+    
+    @Override
+    Map<String, String> validateAuthentication(Authentication authentication) {
+        senderDefaultHttps.expectStatus(
+                new HttpRequest(
+                        url: "https://orbit-secured.herokuapp.com/orbit/public/validateOtp",
+                        method: "POST",
+                        headers: [
+                                "Content-Type" : "application/json",
+                                "Accept"       : "application/json"
+                        ],
+                        body: """{
+	"guid": ${authentication.authenticationData.publicCredentials.get("otpGuid")},
+	"otp": ${authentication.authenticationData.publicCredentials.get("otp")}
+}"""
+                ), 200
+        )
+        return [
+                "otpGuid": authentication.authenticationData.publicCredentials.get("otpGuid"),
+                "phone": authentication.authenticationData.publicCredentials.get("phone")
+        ]
+    }
+
+}

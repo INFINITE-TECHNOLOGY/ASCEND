cba11f9ff324c8f18fab56a86ed290fa4e3dde1a
apryamostanov <anton.pryamostanov@gmail.com>
2020-05-09 00:49:47 +0300

    Progress

--- ascend-web/src/main/groovy/io/infinite/ascend/ConfigInitService.groovy
+++ ascend-web/src/main/groovy/io/infinite/ascend/ConfigInitService.groovy
@@ -50,20 +50,26 @@ class ConfigInitService {
                 publicKey: "30820222300d06092a864886f70d01010105000382020f003082020a02820201009b7e8a8cf855cabb9b3c9e645ae92edb226022b2f04b72aa3f9f6b505b9f2f7d13f8b3fa38e6937b1162288adf6b3056d83b5743afb413f5fa66e18e365a44f876793fbdbbbe0fb2e3bc6ed46b761c90769b4aa5d8e56b504b6005de3c69d30e8f1181972200a8d969a0947644be1d35901027d95796e7e9864a60a7484937770e52a41bd7a2f0c6b958431491cb7a07076870fecb88890ad7bcbe2d27bf8348874cd4b712984d1769376ada5da6c10dcbaed12d4a2a5a0489e37bd4d14a1342297a67e31028032f6118b43b66b9e80f90440830f88c49762562f770cfc8df71cb6ad2a73a880aa1d547391417515b246253bfdd7e84f830630be5825cd96ee702bfd8e39b46d60f9a0ddaa3d4eb820553e6f6ba5309b8ddec3b4ccc32154311af2a3d7a2507c92e39b8019b0eb887bd4931e7aa097276bc0259858435736801ca9f9b49743ed41c6d3c7e59ff5901b4efb97b2672692bf870e7a6771e3dee9a5c058eccab371e2a4481bddfc59d2d7c7cb8158d2994266fe7a7cffcfa7136bb266f67fee3a9fe65a39e9820cd773da218550cff7ff62114f17b219565c041d94011b7dfb13f4c0bd3804cecce514e5a7edafd0cd7dd8c0e3df9a79baa4137708e48e8529f4e5967416d3912e36eb4674422733ff80f2139342baec92065712eb3b99c31753a8f663cd5cf69b4d722f91eddd45e2d883aa5b90c0c08a7af8e6f0203010001"
         ))
         log.info("Initializing config")
-        PrototypeGrant managedEmail = grantRepository.saveAndFlush(new PrototypeGrant(httpMethod: "POST", urlRegex: "https:\\/\\/orbit-secured\\.herokuapp\\.com\\/orbit\\/secured\\/managedEmail"))
-        PrototypeGrant managedSms = grantRepository.saveAndFlush(new PrototypeGrant(httpMethod: "POST", urlRegex: "https:\\/\\/orbit-secured\\.herokuapp\\.com\\/orbit\\/secured\\/managedSms"))
+        PrototypeGrant sendOtpSms = grantRepository.saveAndFlush(new PrototypeGrant(httpMethod: "POST", urlRegex: "https:\\/\\/orbit-secured\\.herokuapp\\.com\\/orbit\\/secured\\/sendOtpSms"))
         PrototypeGrant userGet = grantRepository.saveAndFlush(new PrototypeGrant(httpMethod: "GET", urlRegex: "https:\\/\\/orbit-secured\\.herokuapp\\.com\\/orbit\\/secured\\/user\\/%phone%"))
+        PrototypeGrant userPost = grantRepository.saveAndFlush(new PrototypeGrant(httpMethod: "POST", urlRegex: "https:\\/\\/orbit-secured\\.herokuapp\\.com\\/orbit\\/secured\\/user\\/%phone%"))
         PrototypeGrant adminFindByPhone = grantRepository.saveAndFlush(new PrototypeGrant(httpMethod: "GET", urlRegex: "https:\\/\\/orbit-secured\\.herokuapp\\.com\\/orbit\\/secured\\/admin\\/search\\/findByPhone?%phone%"))
         PrototypeGrant adminFindByEmail = grantRepository.saveAndFlush(new PrototypeGrant(httpMethod: "GET", urlRegex: "https:\\/\\/orbit-secured\\.herokuapp\\.com\\/orbit\\/secured\\/admin\\/search\\/findByEmail?%email%"))
-        PrototypeGrant userPost = grantRepository.saveAndFlush(new PrototypeGrant(httpMethod: "POST", urlRegex: "https:\\/\\/orbit-secured\\.herokuapp\\.com\\/orbit\\/secured\\/user\\/%phone%"))
         PrototypeGrant adminGrant = grantRepository.saveAndFlush(new PrototypeGrant(httpMethod: "POST", urlRegex: "https:\\/\\/orbit-secured\\.herokuapp\\.com\\/orbit\\/secured\\/admin\\/.*"))
+        PrototypeScope legalScope = scopeRepository.saveAndFlush(
+                new PrototypeScope(
+                        name: "onboardingScope",
+                        grants: [
+                                sendOtpSms
+                        ].toSet()
+                )
+        )
         PrototypeScope onboardingScope = scopeRepository.saveAndFlush(
                 new PrototypeScope(
                         name: "onboardingScope",
                         grants: [
                                 userGet,
-                                userPost,
-                                managedSms
+                                userPost
                         ].toSet()
                 )
         )
@@ -72,9 +78,7 @@ class ConfigInitService {
                         name: "adminOnboardingScope",
                         grants: [
                                 adminFindByPhone,
-                                adminFindByEmail,
-                                managedEmail,
-                                managedSms
+                                adminFindByEmail
                         ].toSet()
                 )
         )
@@ -119,6 +123,25 @@ class ConfigInitService {
         PrototypeAuthentication adminAuthentication = authenticationTypeRepository.saveAndFlush(
                 new PrototypeAuthentication(
                         name: "admin"
+                )
+        )
+        PrototypeAuthentication privacyPolicy = authenticationTypeRepository.saveAndFlush(
+                new PrototypeAuthentication(
+                        name: "privacyPolicy"
+                )
+        )
+        PrototypeAuthentication termsAndConditions = authenticationTypeRepository.saveAndFlush(
+                new PrototypeAuthentication(
+                        name: "termsAndConditions"
+                )
+        )
+        PrototypeIdentity legalUser = identityTypeRepository.saveAndFlush(
+                new PrototypeIdentity(
+                        name: "emailOwner",
+                        authentications: [
+                                privacyPolicy,
+                                termsAndConditions
+                        ].toSet()
                 )
         )
         PrototypeIdentity verifiedEmailOwner = identityTypeRepository.saveAndFlush(
@@ -174,6 +197,18 @@ class ConfigInitService {
                 durationSeconds: Duration.ofDays(30).seconds.toInteger(),
                 isRenewable: true
         ))
+        PrototypeAuthorization legalAuthorization = authorizationTypeRepository.saveAndFlush(
+                new PrototypeAuthorization(name: "legalAuthorization",
+                        identities: [
+                                legalUser
+                        ].toSet(),
+                        scopes: [
+                                legalScope
+                        ].toSet(),
+                        durationSeconds: Duration.ofMinutes(30).seconds.toInteger(),
+                        serverNamespace: "OrbitSaaS"
+                )
+        )
         PrototypeAuthorization onboardingScopeAuthorization = authorizationTypeRepository.saveAndFlush(
                 new PrototypeAuthorization(name: "onboardingScopeAuthorization",
                         identities: [
@@ -184,7 +219,10 @@ class ConfigInitService {
                         ].toSet(),
                         durationSeconds: Duration.ofMinutes(30).seconds.toInteger(),
                         serverNamespace: "OrbitSaaS",
-                        refresh: refresh1dayNonRenewable
+                        refresh: refresh1dayNonRenewable,
+                        prerequisites: [
+                                legalAuthorization
+                        ].toSet()
                 )
         )
         PrototypeAuthorization adminOnboardingScopeAuthorization = authorizationTypeRepository.saveAndFlush(
@@ -198,7 +236,10 @@ class ConfigInitService {
                         ].toSet(),
                         durationSeconds: Duration.ofMinutes(30).seconds.toInteger(),
                         serverNamespace: "OrbitSaaS",
-                        refresh: refresh1dayNonRenewable
+                        refresh: refresh1dayNonRenewable,
+                        prerequisites: [
+                                legalAuthorization
+                        ].toSet()
                 )
         )
         PrototypeAuthorization registeredUserScopeAuthorization = authorizationTypeRepository.saveAndFlush(

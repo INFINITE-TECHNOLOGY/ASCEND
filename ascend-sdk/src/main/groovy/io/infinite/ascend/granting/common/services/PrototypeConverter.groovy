5a63e13f53cfe819b05b24de6753733a565c8f63
apryamostanov <anton.pryamostanov@gmail.com>
2020-05-21 06:58:29 +0300

    Progress

--- ascend-sdk/src/main/groovy/io/infinite/ascend/granting/common/services/PrototypeConverter.groovy
+++ ascend-sdk/src/main/groovy/io/infinite/ascend/granting/common/services/PrototypeConverter.groovy
@@ -4,6 +4,8 @@ import io.infinite.ascend.common.entities.*
 import io.infinite.ascend.common.repositories.AuthorizationRepository
 import io.infinite.ascend.common.services.JwtService
 import io.infinite.ascend.granting.configuration.entities.*
+import io.infinite.blackbox.BlackBox
+import io.infinite.carburetor.CarburetorLevel
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.ApplicationContext
 import org.springframework.stereotype.Service
@@ -12,6 +14,7 @@ import java.time.Duration
 import java.time.Instant
 
 @Service
+@BlackBox(level = CarburetorLevel.METHOD)
 class PrototypeConverter {
 
     @Autowired
